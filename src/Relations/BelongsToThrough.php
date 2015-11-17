04f5ec2e4aa1044cfd9b88c931d28590003cfdf5
Rahul Kadyan <hi@znck.me>
2015-11-17 12:13:03 +0530

    get foreign key column from table name

--- src/Relations/BelongsToThrough.php
+++ src/Relations/BelongsToThrough.php
@@ -2,14 +2,23 @@
 
 namespace Znck\Eloquent\Relations;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Query\Expression;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Query\Expression;
+use Illuminate\Support\Str;
 
+/**
+ * Class BelongsToThrough
+ *
+ * @package Znck\Eloquent\Relations
+ * @author Rahul Kadyan <hi@znck.me>
+ */
 class BelongsToThrough extends Relation
 {
+
     /**
      * Column alias for matching eagerly loaded models.
      *
@@ -20,7 +29,7 @@ class BelongsToThrough extends Relation
     /**
      * List of intermediate model instances.
      *
-     * @var array
+     * @var \Illuminate\Database\Eloquent\Model[]
      */
     protected $models;
 
@@ -34,11 +43,10 @@ class BelongsToThrough extends Relation
     /**
      * Create a new instance of relation.
      *
-     * @param  \Illuminate\Database\Eloquent\Builder  $query
-     * @param  \Illuminate\Database\Eloquent\Model  $parent
-     * @param  array  $models
-     * @param  string|null  $localKey
-     * @return void
+     * @param  \Illuminate\Database\Eloquent\Builder $query
+     * @param  \Illuminate\Database\Eloquent\Model $parent
+     * @param  array $models
+     * @param  string|null $localKey
      */
     public function __construct(Builder $query, Model $parent, array $models, $localKey = null)
     {
@@ -57,111 +65,12 @@ class BelongsToThrough extends Relation
     {
         $this->setJoins();
 
-        $this->getQuery()->select([$this->getRelated()->getTable().'.*']);
+        $this->getQuery()->select([ $this->getRelated()->getTable().'.*' ]);
 
         if (static::$constraints) {
             $this->getQuery()->where($this->getQualifiedParentKeyName(), '=', $this->parent[$this->localKey]);
             $this->setSoftDeletes();
         }
-    }
-
-    /**
-     * Set the constraints for an eager load of the relation.
-     *
-     * @param  array  $models
-     * @return void
-     */
-    public function addEagerConstraints(array $models)
-    {
-        $this->getQuery()->addSelect([
-            $this->getParent()->getQualifiedKeyName().' as '.self::RELATED_THROUGH_KEY,
-        ]);
-
-        $this->getQuery()->whereIn($this->getParent()->getQualifiedKeyName(), $this->getKeys($models, $this->localKey));
-    }
-
-    /**
-     * Initialize the relation on a set of models.
-     *
-     * @param  array $models
-     * @param  string  $relation
-     * @return array
-     */
-    public function initRelation(array $models, $relation)
-    {
-        foreach ($models as $model) {
-            $model->setRelation($relation, $this->getRelated());
-        }
-
-        return $models;
-    }
-
-    /**
-     * Match the eagerly loaded results to their parents.
-     *
-     * @param  array  $models
-     * @param  \Illuminate\Database\Eloquent\Collection  $results
-     * @param  string  $relation
-     * @return array
-     */
-    public function match(array $models, Collection $results, $relation)
-    {
-        $dictionary = $this->buildDictionary($results);
-
-        foreach ($models as $model) {
-            $key = $model->{$this->localKey};
-
-            if (isset($dictionary[$key])) {
-                $model->setRelation($relation, $dictionary[$key]);
-            }
-        }
-
-        return $models;
-    }
-
-    /**
-     * Get the results of the relationship.
-     *
-     * @return \Illuminate\Database\Eloquent\Model
-     */
-    public function getResults()
-    {
-        return $this->getQuery()->first();
-    }
-
-    /**
-     * Add the constraints for a relationship count query.
-     *
-     * @param  \Illuminate\Database\Eloquent\Builder  $query
-     * @param  \Illuminate\Database\Eloquent\Builder  $parent
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public function getRelationCountQuery(Builder $query, Builder $parent)
-    {
-        $query->select(new Expression('count(*)'));
-
-        $one = $this->getRelated()->getQualifiedKeyName();
-        $prev = $this->getRelated()->getForeignKey();
-        $alias = null;
-        foreach ($this->models as $model) {
-            if ($this->getParent()->getTable() == $model->getTable()) {
-                $alias = $model->getTable().'_'.time();
-                $other = $alias.'.'.$prev;
-                $query->leftJoin(new Expression($model->getTable().' as '.$alias), $one, '=', $other);
-            } else {
-                $other = $model->getTable().'.'.$prev;
-                $query->leftJoin($model->getTable(), $one, '=', $other);
-            }
-
-            $prev = $model->getForeignKey();
-            $one = $model->getQualifiedKeyName();
-        }
-
-        $key = $this->wrap($this->getQualifiedParentKeyName());
-
-        $query->where(new Expression($alias.'.'.$this->getParent()->getKeyName()), '=', new Expression($key));
-
-        return $query;
     }
 
     /**
@@ -172,14 +81,24 @@ class BelongsToThrough extends Relation
     protected function setJoins()
     {
         $one = $this->getRelated()->getQualifiedKeyName();
-        $prev = $this->getRelated()->getForeignKey();
+        $prev = $this->getForeignKey($this->getRelated());
         foreach ($this->models as $model) {
             $other = $model->getTable().'.'.$prev;
             $this->getQuery()->leftJoin($model->getTable(), $one, '=', $other);
 
-            $prev = $model->getForeignKey();
+            $prev = $this->getForeignKey($model);
             $one = $model->getQualifiedKeyName();
         }
+    }
+
+    /**
+     * Get the underlying query for the relation.
+     *
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder
+     */
+    public function getQuery()
+    {
+        return $this->query;
     }
 
     /**
@@ -197,9 +116,20 @@ class BelongsToThrough extends Relation
     }
 
     /**
+     * Get foreign key column name for the model.
+     *
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @return string
+     */
+    protected function getForeignKey(Model $model)
+    {
+        return Str::singular($model->getTable()).'_id';
+    }
+
+    /**
      * Determine whether the model uses Soft Deletes.
      *
-     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  \Illuminate\Database\Eloquent\Model $model
      * @return bool
      */
     public function hasSoftDeletes(Model $model)
@@ -208,18 +138,117 @@ class BelongsToThrough extends Relation
     }
 
     /**
+     * Set the constraints for an eager load of the relation.
+     *
+     * @param  array $models
+     * @return void
+     */
+    public function addEagerConstraints(array $models)
+    {
+        $this->getQuery()->addSelect([
+            $this->getParent()->getQualifiedKeyName().' as '.self::RELATED_THROUGH_KEY,
+        ]);
+
+        $this->getQuery()->whereIn($this->getParent()->getQualifiedKeyName(), $this->getKeys($models, $this->localKey));
+    }
+
+    /**
+     * Add the constraints for a relationship count query.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder $query
+     * @param  \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder $parent
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder
+     */
+    public function getRelationCountQuery(Builder $query, Builder $parent)
+    {
+        $query->select(new Expression('count(*)'));
+
+        $one = $this->getRelated()->getQualifiedKeyName();
+        $prev = $this->getForeignKey($this->getRelated());
+        $alias = null;
+        foreach ($this->models as $model) {
+            if ($this->getParent()->getTable() == $model->getTable()) {
+                $alias = $model->getTable().'_'.time();
+                $other = $alias.'.'.$prev;
+                $query->leftJoin(new Expression($model->getTable().' as '.$alias), $one, '=', $other);
+            } else {
+                $other = $model->getTable().'.'.$prev;
+                $query->leftJoin($model->getTable(), $one, '=', $other);
+            }
+
+            $prev = $this->getForeignKey($model);
+            $one = $model->getQualifiedKeyName();
+        }
+
+        $key = $this->wrap($this->getQualifiedParentKeyName());
+
+        $query->where(new Expression($alias.'.'.$this->getParent()->getKeyName()), '=', new Expression($key));
+
+        return $query;
+    }
+
+    /**
+     * Get the results of the relationship.
+     *
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function getResults()
+    {
+        return $this->getQuery()->first();
+    }
+
+    /**
+     * Initialize the relation on a set of models.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model[] $models
+     * @param  string $relation
+     * @return array
+     */
+    public function initRelation(array $models, $relation)
+    {
+        foreach ($models as $model) {
+            $model->setRelation($relation, $this->getRelated());
+        }
+
+        return $models;
+    }
+
+    /**
+     * Match the eagerly loaded results to their parents.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model[] $models
+     * @param  \Illuminate\Database\Eloquent\Collection $results
+     * @param  string $relation
+     * @return array
+     */
+    public function match(array $models, Collection $results, $relation)
+    {
+        $dictionary = $this->buildDictionary($results);
+
+        foreach ($models as $model) {
+            $key = $model->{$this->localKey};
+
+            if (isset( $dictionary[$key] )) {
+                $model->setRelation($relation, $dictionary[$key]);
+            }
+        }
+
+        return $models;
+    }
+
+    /**
      * Build model dictionary keyed by the relation's foreign key.
      *
-     * @param  \Illuminate\Database\Eloquent\Collection  $results
+     * @param  \Illuminate\Database\Eloquent\Collection $results
      * @return array
      */
     protected function buildDictionary(Collection $results)
     {
-        $dictionary = [];
+        $dictionary = [ ];
 
         foreach ($results as $result) {
             $dictionary[$result->{static::RELATED_THROUGH_KEY}] = $result;
-            unset($result[static::RELATED_THROUGH_KEY]);
+            unset( $result[static::RELATED_THROUGH_KEY] );
         }
 
         return $dictionary;
--- src/Traits/BelongsToThrough.php
+++ src/Traits/BelongsToThrough.php
@@ -1,40 +1,51 @@
 <?php namespace Znck\Eloquent\Traits;
 
+use Exception;
 use Illuminate\Database\Eloquent\Model;
+use InvalidArgumentException;
 use Znck\Eloquent\Relations\BelongsToThrough as Relation;
 
+/**
+ * Class BelongsToThrough
+ *
+ * @package Znck\Eloquent\Traits
+ * @author Rahul Kadyan <hi@znck.me>
+ */
 trait BelongsToThrough
 {
 
     /**
      * Define a belongs-to-through relationship.
      *
-     * @param string  $related
-     * @param string|array  $through
-     * @param string|null  $localKey
-     *
+     * @param string $related
+     * @param string|array $through
+     * @param string|null $localKey
      * @return \Znck\Eloquent\Relations\BelongsToThrough
+     * @throws \Exception
      */
     public function belongsToThrough($related, $through, $localKey = null)
     {
-        $related = new $related;
-        $models = [];
-        foreach ((array)$through as $key => $model) {
+        if (! $this instanceof Model) {
+            throw new Exception('belongsToThrough can used on '.Model::class.' only.');
+        }
+
+        /** @var \Illuminate\Database\Eloquent\Model $relatedModel */
+        $relatedModel = new $related;
+        $models = [ ];
+        foreach ((array) $through as $key => $model) {
             $object = new $model;
-            if (!($object instanceof Model)) {
-                throw new \InvalidArgumentException(
-                    "Through model should be instance of \\Iluminate\\Database\\Eloquent\\Model."
-                );
+            if (! $object instanceof Model) {
+                throw new InvalidArgumentException('Through model should be instance of '.Model::class.'.');
             }
             $models[] = $object;
         }
 
-        if (empty($through)) {
-            throw new \InvalidArgumentException("Provide one or more through model.");
+        if (empty( $through )) {
+            throw new InvalidArgumentException("Provide one or more through model.");
         }
 
         $models[] = $this;
 
-        return new Relation($related->newQuery(), $this, $models, $localKey);
+        return new Relation($relatedModel->newQuery(), $this, $models, $localKey);
     }
 }
