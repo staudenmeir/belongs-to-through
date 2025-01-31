0d7fcbce021668b28109f20329b172fbbbc36c8f
Daniël de Wit <daniel@danieldewit.nl>
2025-01-31 11:39:50 +0100

    Use alias for deferred provider
    Fixes usage of multiple ide-helper hooks with deferred providers

--- src/IdeHelperServiceProvider.php
+++ src/IdeHelperServiceProvider.php
@@ -6,9 +6,19 @@ use Barryvdh\LaravelIdeHelper\Console\ModelsCommand;
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 use Staudenmeir\BelongsToThrough\IdeHelper\BelongsToThroughRelationsHook;
+use Staudenmeir\EloquentHasManyDeep\IdeHelper\DeepRelationsHook;
 
 class IdeHelperServiceProvider extends ServiceProvider implements DeferrableProvider
 {
+    public function boot(): void
+    {
+        // Laravel only allows a single deferred service provider to claim
+        // responsibility for a given class, interface, or service in the
+        // provides() method. To ensure this provider is properly loaded
+        // when running the ModelsCommand we bind an alias and use that instead.
+        $this->app->alias(ModelsCommand::class, BelongsToThroughRelationsHook::class);
+    }
+
     public function register(): void
     {
         /** @var \Illuminate\Config\Repository $config */
@@ -24,12 +34,12 @@ class IdeHelperServiceProvider extends ServiceProvider implements DeferrableProv
     }
 
     /**
-     * @return list<class-string<\Illuminate\Console\Command>>
+     * @return list<class-string<\Barryvdh\LaravelIdeHelper\Contracts\ModelHookInterface>>
      */
     public function provides(): array
     {
         return [
-            ModelsCommand::class,
+            BelongsToThroughRelationsHook::class,
         ];
     }
 }
